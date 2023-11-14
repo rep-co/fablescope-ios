60c045c472b08de9aee021befedc3a52c064bae3
serhiOS <>
2023-11-14 17:24:32 +0300

    project created

---
+++ fablescope/fablescope/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fablescope
+//
+//  Created by Серега Пират on 14/11/2023.
+//  Copyright © 2023 repCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
