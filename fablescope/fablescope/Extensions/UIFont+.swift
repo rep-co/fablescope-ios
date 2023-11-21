c40762169f2cf9c6379832e8656cbeabd2ff46ab
serhiOS <>
2023-11-21 20:20:48 +0300

    FSCP-1: fix after review

--- fablescope/fablescope/Extensions/UIFont+.swift
+++ fablescope/fablescope/Extensions/UIFont+.swift
@@ -9,5 +9,5 @@
 import UIKit
 
 extension UIFont {
-    static let  pacificoRegular = UIFont(name: "Pacifico-Regular", size: 24)!
+    static let pacificoRegular = UIFont(name: "Pacifico-Regular", size: 24)!
 }
