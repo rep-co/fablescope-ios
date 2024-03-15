9ab80d1307be069f5073f42b6e22bb43e493905c
SerhiOS <sergeyaslonyns@mail.ru>
2024-03-16 00:09:22 +0300

    fix

--- fablescope/fablescope/Extensions/UIColor+.swift
+++ fablescope/fablescope/Extensions/UIColor+.swift
@@ -2,5 +2,5 @@ import UIKit
 
 extension UIColor {
     static let fableGreen = UIColor(named: "fable_green")!
-    static let lightSeaGreen = UIColor(named: "light_sea_green")!
+    static let lightSeaGreen = UIColor(named: "mint_green")!
 }
