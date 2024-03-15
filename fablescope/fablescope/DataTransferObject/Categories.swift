948f8a67734a032ab47512f6f7c7df70323a2e07
SerhiOS <sergeyaslonyns@mail.ru>
2024-03-15 15:16:12 +0300

    создана структура

---
+++ fablescope/fablescope/DataTransferObject/Categories.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+struct Welcome: Codable {
+    let categories: [Category]
+}
+
+struct Category: Codable {
+    let id: Int
+    let name: String
+    let tags: [Tag]
+}
+
+struct Tag: Codable {
+    let id: Int
+    let name: String
+    let description: String
+    let iconSVG: String
+    
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case description
+        case iconSVG = "icon_svg"
+    }
+}
